3bac0793c4a50f5946532f10aba71e4bd80bcea8
Shuai <619724696@qq.com>
2024-11-27 01:48:23 -0500

    docker-compose

--- cart/src/test/java/com/example/cart/CartApplicationTests.java
+++ cart/src/test/java/com/example/cart/CartApplicationTests.java
@@ -21,36 +21,36 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 class CartApplicationTests {
-//
-//	@Autowired
-//	private MockMvc mockMvc;
-//
-//	@Autowired
-//	private CartRepository cartRepository;
-//
-//	@Test
-//	public void testAddItem() throws Exception {
-//		String cartItemJson = "{\"product\": \"Book\", \"quantity\": 2}";
-//		mockMvc.perform(post("/cart/add")
-//						.contentType(MediaType.APPLICATION_JSON)
-//						.content(cartItemJson))
-//				.andExpect(status().isOk())
-//				.andExpect(jsonPath("$.product").value("Book"))
-//				.andExpect(jsonPath("$.quantity").value(2));
-//	}
-//
-//	@Test
-//	public void testGetItem() throws Exception {
-//		CartItem item = new CartItem();
-//		item.setProduct("Laptop");
-//		item.setQuantity(1);
-//		cartRepository.save(item);
-//
-//		mockMvc.perform(get("/cart/" + item.getId()))
-//				.andExpect(status().isOk())
-//				.andExpect(jsonPath("$.product").value("Laptop"))
-//				.andExpect(jsonPath("$.quantity").value(1));
-//	}
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private CartRepository cartRepository;
+
+	@Test
+	public void testAddItem() throws Exception {
+		String cartItemJson = "{\"product\": \"Book\", \"quantity\": 2}";
+		mockMvc.perform(post("/cart/add")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(cartItemJson))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.product").value("Book"))
+				.andExpect(jsonPath("$.quantity").value(2));
+	}
+
+	@Test
+	public void testGetItem() throws Exception {
+		CartItem item = new CartItem();
+		item.setProduct("Laptop");
+		item.setQuantity(1);
+		cartRepository.save(item);
+
+		mockMvc.perform(get("/cart/" + item.getId()))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.product").value("Laptop"))
+				.andExpect(jsonPath("$.quantity").value(1));
+	}
 
 
 }
